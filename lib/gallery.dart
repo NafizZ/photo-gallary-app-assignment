fca99f682c4133fcbdaa6a39a6acf45a1755b705
Shah Nafiz Monjoor <shahnafizmanjoor@gmail.com>
2022-02-04 02:12:30 +0600

    tap to see full view of the photo, also with zoon in & out option

--- lib/gallery.dart
+++ lib/gallery.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:photo_view/photo_view.dart';
 
 class Gallery extends StatefulWidget {
   const Gallery({Key? key}) : super(key: key);
@@ -10,7 +11,27 @@ class Gallery extends StatefulWidget {
 
 class _GalleryState extends State<Gallery> {
 
-  List a = [
+  List photosList = [
+    "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2017/02/01/22/02/mountain-landscape-2031539_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2014/09/14/18/04/dandelion-445228_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2016/08/09/21/54/yellowstone-national-park-1581879_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2016/07/11/15/43/pretty-woman-1509956_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2016/02/13/12/26/aurora-1197753_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2016/11/08/05/26/woman-1807533_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2013/11/28/10/03/autumn-219972_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2017/12/17/19/08/away-3024773_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2017/02/01/22/02/mountain-landscape-2031539_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2014/09/14/18/04/dandelion-445228_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2016/08/09/21/54/yellowstone-national-park-1581879_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2016/07/11/15/43/pretty-woman-1509956_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2016/02/13/12/26/aurora-1197753_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2016/11/08/05/26/woman-1807533_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2013/11/28/10/03/autumn-219972_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2017/12/17/19/08/away-3024773_960_720.jpg",
     "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg",
     "https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171_960_720.jpg",
     "https://cdn.pixabay.com/photo/2017/02/01/22/02/mountain-landscape-2031539_960_720.jpg",
@@ -32,13 +53,45 @@ class _GalleryState extends State<Gallery> {
         title: const Text('Photo Gallery'),
       ),
       body: GridView.builder(
-        itemCount: a.length,
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(mainAxisSpacing: 20, crossAxisSpacing: 20, crossAxisCount: 3),
+        itemCount: photosList.length,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          mainAxisSpacing: 10,
+          crossAxisSpacing: 10,
+          crossAxisCount: 3
+        ),
         itemBuilder: (BuildContext ctx, int index) {
-          return Image.network(a[index]);
+          return GestureDetector(
+            onTap: (){
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ViewImageFullScreen(
+                      image: photosList[index]),
+                ),
+              );
+            },
+            child: Image.network(photosList[index], fit: BoxFit.cover)
+          );
         },
       ),
  // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class ViewImageFullScreen extends StatelessWidget {
+  final String image;
+
+  const ViewImageFullScreen({Key? key, required this.image})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      body: PhotoView(
+        imageProvider: NetworkImage(image),
+      )
     );
   }
 }
