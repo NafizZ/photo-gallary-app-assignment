0e4d275b5bfe6c8eca936a87c37bab41764591b0
Shah Nafiz Monjoor <shahnafizmanjoor@gmail.com>
2022-02-04 22:57:01 +0600

    infinite scroll on the gallery screen implemented

--- lib/gallery.dart
+++ lib/gallery.dart
@@ -1,8 +1,9 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
-import 'package:gallery_app_assignment/http/http_service.dart';
 import 'package:photo_view/photo_view.dart';
 
-bool isLoading = true; 
+bool _isLoading = true; 
+bool _isLoadMoreRunning = false;
 
 class Gallery extends StatefulWidget {
   const Gallery({Key? key}) : super(key: key);
@@ -16,26 +17,57 @@ class _GalleryState extends State<Gallery> {
 
   List photosList = [];
 
-  final HttpService httpService = HttpService();
+  void getFirstImages() async{
 
-  getImages() async{
-    var response = await httpService.getImages();
-    for (var element in response) {
-       photosList.add(element['download_url'].toString());
-    }
+    for( var i = 0 ; i <= 18; i++ ) {
+      var rng = Random(); 
+      var linkOfPhoto = "https://picsum.photos/300/200?random=${(rng.nextInt(999999))}";
+      // var response = await httpService.getImages(rng.nextInt(999999));
+      photosList.add(linkOfPhoto);
+   } 
     
-    print(' lenght of photo list: ${photosList.length}, response: $response');
+    // for (var element in response) {
+    //    photosList.add(element['download_url'].toString());
+    // }
+    
     setState(() {
-      isLoading = false;
+      _isLoading = false;
     });
   }
+
+  void loadMore() async{
+    if(_isLoading == false && _isLoadMoreRunning == false){
+      setState(() {
+        _isLoadMoreRunning = true;
+      });
+
+      for( var i = 0 ; i <= 18; i++ ) {
+        var rng = Random(); 
+        var linkOfPhoto = "https://picsum.photos/300/200?random=${(rng.nextInt(999999))}";
+        // var response = await httpService.getImages(rng.nextInt(999999));
+        photosList.add(linkOfPhoto);
+      } 
+    
+      setState(() {
+        _isLoadMoreRunning = false;
+      });
+    }
+  }
+
+  late ScrollController _controller;
 
   @override
   void initState() {
     super.initState();
-    isLoading = true;
-    getImages();
-    
+    _isLoading = true;
+    getFirstImages();
+    _controller = ScrollController()..addListener(loadMore);
+  }
+
+  @override
+  void dispose() {
+    _controller.removeListener(loadMore);
+    super.dispose();
   }
 
   @override
@@ -46,39 +78,53 @@ class _GalleryState extends State<Gallery> {
 
         title: const Text('Photo Gallery'),
       ),
-      body: isLoading ? const CircularProgressIndicator() : GridView.builder(
-        itemCount: photosList.length,
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          mainAxisSpacing: 10,
-          crossAxisSpacing: 10,
-          crossAxisCount: 3
-        ),
-        itemBuilder: (BuildContext ctx, int index) {
-          return GestureDetector(
-            onTap: (){
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => ViewImageFullScreen(
-                      image: photosList[index]),
+      body: _isLoading 
+        ? const Center(child: CircularProgressIndicator())
+        : Column(
+          children: [
+            Expanded(
+              child: GridView.builder(
+                controller: _controller,
+                itemCount: photosList.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  mainAxisSpacing: 10,
+                  crossAxisSpacing: 10,
+                  crossAxisCount: 3,
                 ),
-              );
-            },
-            child: Image.network(photosList[index], fit: BoxFit.cover,
-               errorBuilder: (BuildContext context, Object exception, StackTrace? stackTrace) {
-                // Appropriate logging or analytics, e.g.
-                // myAnalytics.recordError(
-                //   'An error occurred loading "https://example.does.not.exist/image.jpg"',
-                //   exception,
-                //   stackTrace,
-                // );
-                return const Text('ð¢');
-              },
+                itemBuilder: (BuildContext ctx, int index) {
+                  return GestureDetector(
+                    onTap: (){
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ViewImageFullScreen(
+                              image: photosList[index]),
+                        ),
+                      );
+                    },
+                    child: Image.network(photosList[index], fit: BoxFit.cover,
+                      errorBuilder: (BuildContext context, Object exception, StackTrace? stackTrace) {
+                        // Appropriate logging or analytics, e.g.
+                        // myAnalytics.recordError(
+                        //   'An error occurred loading "https://example.does.not.exist/image.jpg"',
+                        //   exception,
+                        //   stackTrace,
+                        // );
+                        return const Text('ð¢');
+                      },
+                    ),
+                  );
+                },
+              ),
             ),
-          );
-        },
-      ),
- // This trailing comma makes auto-formatting nicer for build methods.
+            _isLoadMoreRunning == true ? const Padding(
+                    padding: EdgeInsets.only(top: 20, bottom: 40),
+                    child: Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                  ) : Container(),
+          ],
+        ) 
     );
   }
 }
